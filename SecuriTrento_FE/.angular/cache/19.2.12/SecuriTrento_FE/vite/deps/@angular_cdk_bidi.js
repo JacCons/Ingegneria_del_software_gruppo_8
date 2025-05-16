edd9792e726c02782d3c5608d579c6328d18138b
cornelGrg <grigor2003@gmail.com>
2025-05-16 14:37:15 +0200

    [fix] installed angular inside project's directory

---
+++ SecuriTrento_FE/.angular/cache/19.2.12/SecuriTrento_FE/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-SX6W4NLU.js";
+import "./chunk-VD7QXDYS.js";
+import "./chunk-7UBB22UG.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ SecuriTrento_FE/.angular/cache/19.2.12/SecuriTrento_FE/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,15 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-3V353EZQ.js";
+import "./chunk-EQMVXGWO.js";
+import "./chunk-VD7QXDYS.js";
+import "./chunk-7UBB22UG.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
