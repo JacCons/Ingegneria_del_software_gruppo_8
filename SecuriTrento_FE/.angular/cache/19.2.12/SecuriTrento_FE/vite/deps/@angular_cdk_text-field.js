4342f82acbbaf5923f4a874f1bad5bb62aa38659
FlsLeonardo <leonardofalsarolo@gmail.com>
2025-05-18 10:30:02 +0200

    [fix] fixed package.json

---
+++ SecuriTrento_FE/.angular/cache/19.2.12/SecuriTrento_FE/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,16 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-43WKUKIN.js";
+import "./chunk-JYJSKRTX.js";
+import "./chunk-BSDSC6M5.js";
+import "./chunk-UYOORBAR.js";
+import "./chunk-Y6JQRPRR.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
---
+++ SecuriTrento_FE/.angular/cache/19.2.12/SecuriTrento_FE/vite/deps/@angular_material_icon.js
@@ -0,0 +1,35 @@
+import {
+  ICON_REGISTRY_PROVIDER,
+  ICON_REGISTRY_PROVIDER_FACTORY,
+  MAT_ICON_DEFAULT_OPTIONS,
+  MAT_ICON_LOCATION,
+  MAT_ICON_LOCATION_FACTORY,
+  MatIcon,
+  MatIconModule,
+  MatIconRegistry,
+  getMatIconFailedToSanitizeLiteralError,
+  getMatIconFailedToSanitizeUrlError,
+  getMatIconNameNotFoundError,
+  getMatIconNoHttpProviderError
+} from "./chunk-BC7RDQKU.js";
+import "./chunk-KT4UR2FA.js";
+import "./chunk-X46CUKKG.js";
+import "./chunk-JYJSKRTX.js";
+import "./chunk-57WYTN4D.js";
+import "./chunk-BSDSC6M5.js";
+import "./chunk-UYOORBAR.js";
+import "./chunk-Y6JQRPRR.js";
+export {
+  ICON_REGISTRY_PROVIDER,
+  ICON_REGISTRY_PROVIDER_FACTORY,
+  MAT_ICON_DEFAULT_OPTIONS,
+  MAT_ICON_LOCATION,
+  MAT_ICON_LOCATION_FACTORY,
+  MatIcon,
+  MatIconModule,
+  MatIconRegistry,
+  getMatIconFailedToSanitizeLiteralError,
+  getMatIconFailedToSanitizeUrlError,
+  getMatIconNameNotFoundError,
+  getMatIconNoHttpProviderError
+};
